94fdf50a2adc5b4c0fa538dd419ccb19c262741f
tobexcode <tobexalox@gmail.com>
2024-08-07 20:44:50 +0100

    first commit3

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import fs from "fs";
 import path from "path";
 
 export default defineConfig({
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "localhost-key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "localhost.pem")),
-    },
-    host: "0.0.0.0",
-    port: 5174, // or any port you prefer
-  },
+  // server: {
+  //   https: {
+  //     key: fs.readFileSync(path.resolve(__dirname, "localhost-key.pem")),
+  //     cert: fs.readFileSync(path.resolve(__dirname, "localhost.pem")),
+  //   },
+  //   host: "0.0.0.0",
+  //   port: 5174, // or any port you prefer
+  // },
 });
